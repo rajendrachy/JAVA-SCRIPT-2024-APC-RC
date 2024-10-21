e715e0e11b720fde2ce17d78153de48b90270afe
rajendrachy <chyrajendra32@gmail.com>
2024-10-22 00:07:44 +0530

    update

---
+++ JAVASCRIPT RC 2024/EXAM ST 2/JSON API/script.js
@@ -0,0 +1,33 @@
+// Destructuring of the Objects
+
+// const course = {
+//     coursename : "Js in Hindi",
+//     price : "999",
+//     courseInstructor : "Rajendra",
+// }
+
+// const {courseInstructor} = course
+// console.log(courseInstructor)
+
+
+
+// const navbar = ({company}) => {
+//   console.log(company)
+// }
+
+// navbar({company : "Rajendra"});
+
+
+
+
+
+// ___________JSON_______________
+
+// {
+//     "name" : "Rajendra",
+//     "coursename ": "Js in HIndi",
+//     "price" : "free",
+// }
+
+
+
---
+++ JAVASCRIPT RC 2024/PART 61pt1/APIsss/script.js
@@ -0,0 +1,58 @@
+// EP - 69
+
+// we can't use single code in the JSON file it gives an error
+// const user = {   
+//     "userId": 1, // Key : value 
+//     "id": 1, // In JSON we must use double code " " in the every Key & value it is rule in JSON
+//     "title": "delectus aut autem",
+//     "completed": false // last comma is not allowed but in JS last comma is allowed
+//   }
+
+
+
+//________START_______
+// let userData;
+// // VISTI jsonplaceholder website
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json =>  {
+//         console.log("Got the data");
+//         console.log(json)
+//         userData = json
+//       })
+//     // GET request
+//       // watch ep-> 69  time: 29:25 sec
+// // GET request -> data maganea k liya from backend
+// // POST request -> To create
+// // PUT AND PATCH -> To Modify something
+
+
+
+
+// _________GET Request____________
+// _____DOG API________
+
+const image = document.querySelector('img')
+const button = document.querySelector('button')
+// fetch('https://dog.ceo/api/breeds/image/random')
+//      .then(response => response.json())
+//        .then(json =>  {
+//         //  console.log("Got the data");
+//         //  console.log(json.message)
+//          image.src =  json.message
+//        })
+
+
+button.addEventListener('click', () => {
+
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(json => {
+      //  console.log("Got the data");
+      //  console.log(json.message)
+      image.src = json.message
+    })
+})
+
+
+
